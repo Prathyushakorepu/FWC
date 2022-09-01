dee857e6efd98145f9dc704a88fd5e127596bc7b
Prathyushakorepu <111723616+Prathyushakorepu@users.noreply.github.com>
2022-09-01 16:34:54 +0530

    Update main.c

--- Avr-gcc_assignment-1/codes/main.c
+++ Avr-gcc_assignment-1/codes/main.c
@@ -1,6 +1,5 @@
-//Turns LED on and off
+//XOR USING NOR GATES
 #include <avr/io.h>
-//#include <util/delay.h>
 #include <stdbool.h>
  
 int main (void)
@@ -8,15 +7,16 @@ int main (void)
    bool x0,x1,x2,x3,Q;
    bool a=1,b=0;
 
-   DDRD=0b00000100;
-   DDRB=0b11111100;
+   DDRD=0b00000100;   //declaring pin 2 as output pin in portD
+   DDRB=0b11111100;   //dlclaring pins 0 and 1 of portB as inputs
 
+ //Equations
    x0=(!a);
    x1=(!b);
    x2=(!((x0) || (x1)));
    x3=(!((a) || (b)));
    Q=(!((x2) || (x3)));
-
+//Giving Q to pin 2 of PORTD
      PORTD=(Q << 2);
 
       return 0;
