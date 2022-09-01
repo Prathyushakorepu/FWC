36da8e950716b5896afe021d2a9f8d057bc6262e
Prathyushakorepu <111723616+Prathyushakorepu@users.noreply.github.com>
2022-09-01 15:45:24 +0530

    Add files via upload

---
+++ Avr-gcc_assignment-1/codes/main.c
@@ -0,0 +1,29 @@
+//Turns LED on and off
+#include <avr/io.h>
+//#include <util/delay.h>
+#include <stdbool.h>
+ 
+int main (void)
+{
+   bool x0,x1,x2,x3,Q;
+   bool a=1,b=0;
+
+   DDRD=0b00000100;
+   DDRB=0b11111100;
+
+   x0=(!a);
+   x1=(!b);
+   x2=(!((x0) || (x1)));
+   x3=(!((a) || (b)));
+   Q=(!((x2) || (x3)));
+
+     PORTD=(Q << 2);
+
+     
+	
+  
+
+  
+  return 0;
+
+}
